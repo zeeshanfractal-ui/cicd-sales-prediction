b36892407bc918f30f4a8169b445e39ae1216356
Syedzeeshan Husain <syedzeeshan.husain@fractal.ai>
2025-11-07 11:38:35 +0530

    train model updated

--- train_model.py
+++ train_model.py
@@ -12,6 +12,7 @@ y = data["Sales"]
 # Split train/test
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=42)
 
+print('model is ready to train')
 # Train model
 model = LinearRegression()
 model.fit(X_train, y_train)
